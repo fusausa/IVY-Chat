8b19f3964c921d0118306d8682662aa9fcf2cc22
LAPTOP-1HOQUBN0\239004 <fyuanpo@gmail.com>
2024-06-19 10:52:59 +0900

    imageフォルダに画像追加しました

---
+++ menu/menu.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>IVY CHAT メニュー</title>
+    <link rel="stylesheet" type="text/css" href="menu.css">
+</head>
+<body>
+    <header>
+    <img src="../image/logo.png" alt="Logo" class="logo">
+    </header>
+</body>
+</html>
