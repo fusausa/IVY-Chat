e1fa599e142d3ccfea4911250bc985e72e25bb59
LAPTOP-1HOQUBN0\239004 <fyuanpo@gmail.com>
2024-06-20 14:18:16 +0900

    メニュー画面　Room作成

--- menu/menu.php
+++ menu/menu.php
@@ -9,5 +9,12 @@
     <header>
     <img src="../image/logo.png" alt="Logo" class="logo">
     </header>
-</body>
+
+    <div class="Room">
+        <div class="Room1">Room1</div>
+        <div class="Room2">Room2</div>
+        <div class="Room3">Room3</div>
+        <div class="Room4">Room4</div>
+    </div>
+    </body>
 </html>
